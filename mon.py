cb7994fbf0d8ad222d87e01672b9d0c0caab232d
KEVIN FORD <new_era@KEVINs-MBP.hsd1.wa.comcast.net>
2017-07-11 12:08:11 -0700

    changes

--- mon.py
+++ mon.py
@@ -7,19 +7,28 @@ for i in  range(len(a)):
 	print sum
 
 
- # This counts all odd numbers.
-for num in range(1,1001):
- 		if (num % 3) == 0:
-			print num
+#  # This counts all odd numbers.
+for num in range(1,1001,2):
+		print num
 
 
 
-
+# multiples
 
 # Looping by 5.
 for x in xrange(5,1000001,5):
 	print x
 	
+
+
+# average
+
+a = [1, 2, 5, 10, 255, 3]
+sum = 0
+for i in  a:
+	sum += i
+	
+	print sum/len(a)
 
 
 
