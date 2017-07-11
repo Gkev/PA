ac681235d88c72c027d2755324a9df0f245f3552
KEVIN FORD <new_era@KEVINs-MBP.hsd1.wa.comcast.net>
2017-07-11 10:16:54 -0700

    multiples sum average

---
+++ mon.py
@@ -0,0 +1,26 @@
+# Find sum.
+
+a = [1, 2, 5, 10, 255, 3]
+sum = 0
+for i in  range(len(a)):
+	sum += i
+	print sum
+
+
+ # This counts all odd numbers.
+for num in range(1,1001):
+ 		if (num % 3) == 0:
+			print num
+
+
+
+
+
+# Looping by 5.
+for x in xrange(5,1000001,5):
+	print x
+	
+
+
+
+	
